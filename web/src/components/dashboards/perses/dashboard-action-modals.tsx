8f67ce4fa56122d85debce09e52aa2887e337a2f
Gabriel Bernal <gbernal@redhat.com>
2026-03-05 08:53:47 +0100

    fix: include coo-0.4 fixes
    Signed-off-by: Gabriel Bernal <gbernal@redhat.com>

--- web/src/components/dashboards/perses/dashboard-action-modals.tsx
+++ web/src/components/dashboards/perses/dashboard-action-modals.tsx
@@ -50,11 +50,11 @@ import { t_global_spacer_200, t_global_font_weight_200 } from '@patternfly/react
 import { useNavigate } from 'react-router-dom-v5-compat';
 import { usePerspective, getDashboardUrl } from '../../hooks/usePerspective';
 
-const formGroupStyle = {
+export const formGroupStyle = {
   fontWeight: t_global_font_weight_200.value,
 } as React.CSSProperties;
 
-const LabelSpacer = () => {
+export const LabelSpacer = () => {
   return <div style={{ paddingBottom: t_global_spacer_200.value }} />;
 };
 
@@ -208,7 +208,7 @@ export const DuplicateActionModal = ({ dashboard, isOpen, onClose }: ActionModal
     return allProjects[0] || '';
   }, [dashboard, editableProjects, allProjects]);
 
-  const { schema: validationSchema } = useDashboardValidationSchema(defaultProject, t);
+  const { schema: validationSchema } = useDashboardValidationSchema(t, defaultProject);
 
   const form = useForm<CreateDashboardValidationType>({
     resolver: validationSchema
